eb4e26d1d48a1eef5e56796a2db96c70f8194e31
Tina Ostrander <tina.j.ostrander@gmail.com>
2020-02-27 08:50:43 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *  File name & path
+ *  Author
+ *  Date
+ *  Description
+ */
+
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <style>
+        .err {
+            color: darkred;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+
+    <h1>Login Page</h1>
+
+    <form action="#" method="post">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" name="username" >
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" >
+        </div>
+        <span class="err">Incorrect login</span><br>
+
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+
+</div>
+
+<script src="//code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
